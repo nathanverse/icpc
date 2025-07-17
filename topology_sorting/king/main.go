5ed6a9236a26c1e5bff9b3fbf34f8ac6c26c8a25
spartan-vutran <vu.tran@c0x12c.com>
2025-07-17 08:42:30 +0700

    chore: done build segments

--- topology_sorting/king/main.go
+++ topology_sorting/king/main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"container/list"
 	"fmt"
-	avl "github.com/emirpasic/gods/trees/avltree"
+	llq "github.com/emirpasic/gods/queues/linkedlistqueue"
+	"sort"
 )
 
 type Point struct {
@@ -11,8 +12,36 @@ type Point struct {
 }
 
 func isOverlap(x, y, x1, y1 int) bool {
-
+	return x1 <= y
 }
+
+func merge(x, y, x1, y1 int) []int {
+	res := make([]int, 2)
+	res[0] = x
+
+	if y < y1 {
+		res[1] = y1
+	} else {
+		res[1] = y
+	}
+
+	return res
+}
+
+type Segments [][]int
+
+func (s Segments) Len() int {
+	return len(s)
+}
+
+func (s Segments) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s Segments) Less(i, j int) bool {
+	return s[i][0] < s[j][0]
+}
+
 func buildSegments(allowedSegmentNum int) (map[int][][]int, error) {
 	allowedSegments := make(map[int][][]int)
 	for i := 0; i < allowedSegmentNum; i++ {
@@ -29,12 +58,46 @@ func buildSegments(allowedSegmentNum int) (map[int][][]int, error) {
 		allowedSegments[r] = append(allowedSegments[r], []int{x, y})
 	}
 
-	// Sort allowedSegments
-	for _, segments := range allowedSegments {
-
+	if len(allowedSegments) == 0 {
+		return allowedSegments, nil
 	}
 
-	return allowedSegments
+	// Sort allowedSegments
+	for _, segments := range allowedSegments {
+		sort.Sort(Segments(segments))
+	}
+
+	// Merge segments
+	for _, segments := range allowedSegments {
+		if len(segments) <= 1 {
+			continue
+		}
+
+		lastSegment := segments[0]
+		mergedSegments := make([][]int, 0)
+		mergedSegments = append(mergedSegments, lastSegment)
+		for i := 1; i < len(segments); i++ {
+			if isOverlap(lastSegment[1], lastSegment[1], segments[i][0], segments[i][1]) {
+				mergedSegment := merge(lastSegment[0], lastSegment[1], segments[i][0], segments[i][1])
+				lastSegment[0] = mergedSegment[0]
+				lastSegment[1] = mergedSegment[1]
+				continue
+			}
+
+			mergedSegments = append(mergedSegments, segments[i])
+			lastSegment = mergedSegments[len(segments)-1]
+		}
+	}
+
+	return allowedSegments, nil
+}
+
+func isAllowed(point Point, allowedSegments map[int][][]int) bool {
+	segments, ok := allowedSegments[point.x]
+	if !ok {
+		return false
+	}
+
 }
 
 func main() {
@@ -44,10 +107,27 @@ func main() {
 		return
 	}
 
+	if start.x == des.x && start.y == des.y {
+		fmt.Print(0)
+	}
+
 	var allowedSegmentNum int
 	_, err = fmt.Scan(&allowedSegmentNum)
 	if err != nil {
 		return
 	}
 
+	allowedSegments, err := buildSegments(allowedSegmentNum)
+	if err != nil {
+		return
+	}
+
+	d := make(map[Point]int)
+	q := llq.New()
+	q.Enqueue(start)
+	for !q.Empty() {
+		visitedNode, _ := q.Dequeue()
+		visitedPoint, _ := visitedNode.(*Point)
+		pushAllowedCells(visitedPoint, allowedSegments)
+	}
 }
