15063a9ed15a665369c7e5384b3079e5f662fbf9
spartan-vutran <vu.tran@c0x12c.com>
2025-08-30 21:05:16 +0700

    feat: add lazy segment tree

--- segment_tree/sum/main.go
+++ segment_tree/sum/main.go
@@ -2,7 +2,24 @@ package main
 
 import "fmt"
 
-// Build tree
+type RangeSum struct {
+	lazySegTree []int
+	segTree     []int
+	arr         []int
+}
+
+func newRangeSum(arr []int) *RangeSum {
+	segmentTree := make([]int, 2*len(arr))
+	lazySegTree := make([]int, 2*len(arr))
+	buildTree(arr, segmentTree, 0, len(arr)-1, 0)
+
+	return &RangeSum{lazySegTree, segmentTree, arr}
+}
+
+func (rs *RangeSum) FindSum(from, to int) int {
+	return rs.findSum(0, len(rs.arr)-1, from, to, 0)
+}
+
 func buildTree(arr []int, segmentTree []int, left, right, index int) {
 	if left == right {
 		segmentTree[index] = arr[left]
@@ -15,52 +32,110 @@ func buildTree(arr []int, segmentTree []int, left, right, index int) {
 	segmentTree[index] = segmentTree[2*index+1] + segmentTree[2*index+2]
 }
 
-func findSum(segmentTree []int, left, right, from, to, index int) int {
-	if left >= from && right <= to {
-		return segmentTree[index]
-	}
-
+func (rs *RangeSum) findSum(left, right, from, to, index int) int {
 	if right < from || left > to {
 		return 0
 	}
 
+	if rs.lazySegTree[index] != 0 {
+		rs.segTree[index] += (right - left + 1) * rs.lazySegTree[index]
+		if 2*index+1 < len(rs.lazySegTree) {
+			rs.lazySegTree[2*index+1] = rs.lazySegTree[index]
+		}
+
+		if 2*index+2 < len(rs.lazySegTree) {
+			rs.lazySegTree[2*index+2] = rs.lazySegTree[index]
+		}
+
+		rs.lazySegTree[index] = 0
+	}
+
+	if left >= from && right <= to {
+		return rs.segTree[index]
+	}
+
 	mid := left + (right-left)/2
-	return findSum(segmentTree, left, mid, from, to, 2*index+1) + findSum(segmentTree, mid+1, right, from, to, 2*index+2)
+	return rs.findSum(left, mid, from, to, 2*index+1) + rs.findSum(mid+1, right, from, to, 2*index+2)
 }
 
-func updateTree(arr []int, segmentTree []int, from, to, index, uIndex, nValue int) {
+func (rs *RangeSum) UpdateTree(uIndex, nValue int) {
+	rs.updateTree(0, len(rs.arr)-1, 0, uIndex, nValue)
+}
+
+func (rs *RangeSum) updateTree(from, to, index, uIndex, nValue int) {
 	if uIndex > to || uIndex < from {
 		return
 	}
 
 	if from == to {
-		segmentTree[index] = nValue
-		arr[uIndex] = nValue
+		rs.segTree[index] = nValue
+		rs.arr[uIndex] = nValue
 		return
 	}
 
 	mid := from + (to-from)/2
 	if mid >= uIndex {
-		updateTree(arr, segmentTree, from, mid, 2*index+1, uIndex, nValue)
+		rs.updateTree(from, mid, 2*index+1, uIndex, nValue)
 	} else {
-		updateTree(arr, segmentTree, mid+1, to, 2*index+2, uIndex, nValue)
+		rs.updateTree(mid+1, to, 2*index+2, uIndex, nValue)
 	}
 
-	segmentTree[index] = segmentTree[2*index+1] + segmentTree[2*index+2]
+	rs.segTree[index] = rs.segTree[2*index+1] + rs.segTree[2*index+2]
 }
 
-func updateIncrementTree(lazyTree []int, segmentTree []int, from, to, increment int) {
+func (rs *RangeSum) UpdateIncrementTree(from, to, increment int) {
+	rs.updateIncrementTree(0, len(rs.arr)-1, 0, from, to, increment)
+}
 
+func (rs *RangeSum) updateIncrementTree(left, right, index, from, to, increment int) {
+	if right < from || left > to {
+		return
+	}
+
+	if rs.lazySegTree[index] != 0 {
+		rs.segTree[index] += (right - left + 1) * rs.lazySegTree[index]
+		if 2*index+1 < len(rs.lazySegTree) {
+			rs.lazySegTree[2*index+1] = rs.lazySegTree[index]
+		}
+
+		if 2*index+2 < len(rs.lazySegTree) {
+			rs.lazySegTree[2*index+2] = rs.lazySegTree[index]
+		}
+
+		rs.lazySegTree[index] = 0
+	}
+
+	if left >= from && right <= to {
+		rs.segTree[index] += (right - left + 1) * increment
+		if 2*index+1 < len(rs.lazySegTree) {
+			rs.lazySegTree[2*index+1] = increment
+		}
+
+		if 2*index+2 < len(rs.lazySegTree) {
+			rs.lazySegTree[2*index+2] = increment
+		}
+
+		return
+	}
+
+	mid := left + (right-left)/2
+	rs.updateIncrementTree(left, mid, 2*index+1, from, to, increment)
+	rs.updateIncrementTree(mid+1, right, 2*index+2, from, to, increment)
+
+	rs.segTree[index] = rs.segTree[2*index+1] + rs.segTree[2*index+2]
+}
+
+func (rs *RangeSum) Print() {
+	fmt.Printf("Lazy segment tree: %v\n", rs.lazySegTree)
+	fmt.Printf("Segment tree: %v\n", rs.segTree)
+	fmt.Printf("Array: %v\n", rs.arr)
 }
 
 func main() {
-	arr := []int{1, 5, 9, 2}
-	segmentTree := make([]int, 2*len(arr))
-
-	buildTree(arr, segmentTree, 0, len(arr)-1, 0)
-
-	updateTree(arr, segmentTree, 0, len(arr)-1, 0, 2, 3)
-
-	fmt.Println(arr)
-	fmt.Println(segmentTree)
+	arr := []int{3, 5, 8, 10, 4, 9, 3}
+	rangeSum := newRangeSum(arr)
+	rangeSum.UpdateIncrementTree(0, 3, 3)
+	fmt.Printf("Sum: %d\n", rangeSum.FindSum(0, 2))
+	rangeSum.Print()
+	//fmt.Println(rangeSum.FindSum(1, 3))
 }
